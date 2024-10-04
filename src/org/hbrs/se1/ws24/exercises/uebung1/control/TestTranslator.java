bbb22e435915c5407ec215fff1d686f8e80cff25
ki-fer <adrian.kiefer@smail.inf.h-brs.de>
2024-10-04 11:59:01 +0200

    Test Backup

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/TestTranslator.java
+++
@@ -1,8 +0,0 @@
-package src.org.hbrs.se1.ws24.exercises.uebung1.control;
-
-public class TestTranslator {
-    public static void main(String[] args) {
-        GermanTranslator a1 = new GermanTranslator();
-        System.out.println(a1.translateNumber(8));
-    }
-}
---
+++ test Backup/org/hbrs/se1/ws24/tests/uebung1/A.java
@@ -0,0 +1,3 @@
+package test.org.hbrs.se1.ws24.tests.uebung1;
+
+git help -a
